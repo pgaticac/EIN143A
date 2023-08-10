fab203f3fafe6c86d7c261c339b5ec0128a9c55e
pgaticac <pamela.gatica@usm.cl>
2023-08-10 11:39:02 -0400

    Semana 2 : Orientación a Objetos y Multicapas

---
+++ Clases/Semana2/Clases/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MisClases;
+
+namespace Presentacion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona p = new Persona();
+            p.Nombre = "Pedro";
+            p.Apellido = "Parker";
+            p.FechaNacimiento = new DateTime(1999, 5, 14);
+
+            Persona p1 = new Persona("Bruno", "Diaz");
+            p1.FechaNacimiento = new DateTime(1970, 9, 24);
+
+            Console.WriteLine(p.Nombre + " " + p.Apellido);
+            Console.WriteLine(p1.Nombre + " " + p1.Apellido);
+
+            Console.WriteLine(p);
+            Console.WriteLine(p1);
+
+            Console.WriteLine(p.Edad);
+            Console.WriteLine(p1.Edad);
+
+            Console.WriteLine(p.FechaNacimiento.ToString("dd/MM/yyyy"));
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Clases/Semana2/MisClases/Persona.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MisClases
+{
+    public class Persona
+    {
+        private string _nombre;
+        private string _apellido;
+        private DateTime _fechaNacimiento;
+
+        public Persona()
+        {
+
+        }
+        public Persona(string nombre, string apellido)
+        {
+            this.Nombre = nombre;
+            this.Apellido = apellido;
+        }
+
+        //CTRL+R , CTRL+E encapsula un campo
+        public string Nombre { get => _nombre; set => _nombre = value; }
+        public string Apellido { get => _apellido; set => _apellido = value; }
+        public DateTime FechaNacimiento { get => _fechaNacimiento; set => _fechaNacimiento = value; }
+
+        public int Edad
+        {
+            get
+            {
+                int edad = DateTime.Today.Year - this.FechaNacimiento.Year;
+                return edad;
+                //Ctrl+K, Ctrl+D ordena el código
+            }
+
+        }
+
+
+
+
+        public override string ToString()
+        {
+            return this.Nombre + " " + this.Apellido;
+        }
+
+
+    }
+}
