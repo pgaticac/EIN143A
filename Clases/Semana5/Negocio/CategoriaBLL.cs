63f1b1e93a01248fe1f2eb26a7fb6d2bc3bdb10e
pgaticac <pamela.gatica@usm.cl>
2023-09-07 11:05:47 -0300

    Entity Framework: Eliminar, Editar y Filtrar

--- Clases/Semana5/Negocio/CategoriaBLL.cs
+++ Clases/Semana5/Negocio/CategoriaBLL.cs
@@ -35,6 +35,13 @@ namespace Negocio
             return categorias;
         }
 
+        public List<Categoria> GetFilter(string filtro)
+        {
+            List<Categoria> categorias = new List<Categoria>();
+            categorias = bde.Categoria.Where(c => c.Nombre.Contains(filtro)).ToList();
+            return categorias;
+        }
+
         public bool Find(string nombre)
         {
             Categoria cat = bde.Categoria.FirstOrDefault(c => c.Nombre == nombre);
@@ -47,5 +54,21 @@ namespace Negocio
                 return true;
             }
         }
+
+        public void Delete(Categoria categoria)
+        {
+            bde.Categoria.Remove(categoria);
+            bde.SaveChanges();
+        }
+
+        public void Edit(Categoria categoria)
+        {   
+            //La búsqueda debe ser por la clave primaria (u otro cambio que no haya sido editado)
+            Categoria cat = bde.Categoria.FirstOrDefault(c => c.Id == categoria.Id);
+            cat.Nombre = categoria.Nombre;
+            bde.SaveChanges();
+        }
+
+
     }
 }
--- Clases/Semana5/Presentacion/MainWindow.xaml.cs
+++ Clases/Semana5/Presentacion/MainWindow.xaml.cs
@@ -24,16 +24,23 @@ namespace Presentacion
     {
 
         CategoriaBLL catBll = new CategoriaBLL();
+        Categoria c;
         public MainWindow()
         {
             InitializeComponent();
             CargarCategorias();
         }
 
-        private void CargarCategorias()
+        public  void CargarCategorias()
         {
             lstCategorias.ItemsSource = null;
             lstCategorias.ItemsSource = catBll.GetAll();
+        }
+
+        public void CargarCategorias(string filtro)
+        {
+            lstCategorias.ItemsSource = null;
+            lstCategorias.ItemsSource = catBll.GetFilter(filtro);
         }
 
         private void BtnNuevaCategoria_Click(object sender, RoutedEventArgs e)
@@ -48,6 +55,55 @@ namespace Presentacion
 
             CargarCategorias();
 
+        }
+
+        private void BtnEliminar_Click(object sender, RoutedEventArgs e)
+        {
+            if (lstCategorias.SelectedIndex == -1)
+            {
+                MessageBox.Show("Selecciona categoría a eliminar");
+            }
+            else
+            {
+                Categoria c = (Categoria)lstCategorias.SelectedItem;
+                catBll.Delete(c);
+                CargarCategorias();
+
+            }
+            
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (lstCategorias.SelectedIndex == -1)
+            {
+                MessageBox.Show("Selecciona categoría a eliminar");
+            }
+            else
+            {
+                c = (Categoria)lstCategorias.SelectedItem;
+                txtNuevaCategoria.Text = c.Nombre;
+
+            }
+        }
+
+        private void BtnEditarCategoria_Click(object sender, RoutedEventArgs e)
+        {
+            c.Nombre = txtNuevaCategoria.Text;
+            catBll.Edit(c);
+            CargarCategorias();
+
+            txtNuevaCategoria.Text = String.Empty;
+
+        }
+
+        private void BtnFiltrar_Click(object sender, RoutedEventArgs e)
+        {
+            string filtro = txtFiltro.Text;
+            CargarCategorias(filtro);
+
+            
         }
     }
 }
